579bbce0655c4050d4b2a298206dcd36b8f0189b
salmanrs <49604461+salmanrs@users.noreply.github.com>
2025-09-13 13:22:22 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Simple installer
+
+curl -o MTProtoProxyInstall.sh -L https://raw.githubusercontent.com/yourusername/MTProtoProxyAdvanced/master/MTProtoProxyInstall.sh
+chmod +x MTProtoProxyInstall.sh
+./MTProtoProxyInstall.sh
